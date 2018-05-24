06b9b01e3f763685d17d979a567d7eb075f2371d
blay09 <blay09@hotmail.de>
2018-05-24 08:36:36 +0200

    Fix priority button showing up on filtered hopper
    #45

--- src/main/java/net/blay09/mods/refinedrelocation/client/gui/GuiRootFilter.java
+++ src/main/java/net/blay09/mods/refinedrelocation/client/gui/GuiRootFilter.java
@@ -26,111 +26,121 @@ import net.minecraft.util.ResourceLocation;
 
 public class GuiRootFilter extends GuiContainerMod<ContainerRootFilter> implements IFilterPreviewGui {
 
-	private static final ResourceLocation TEXTURE = new ResourceLocation(RefinedRelocation.MOD_ID, "textures/gui/root_filter.png");
-	private static final int UPDATE_INTERVAL = 20;
+    private static final ResourceLocation TEXTURE = new ResourceLocation(RefinedRelocation.MOD_ID, "textures/gui/root_filter.png");
+    private static final ResourceLocation TEXTURE_NO_PRIORITY = new ResourceLocation(RefinedRelocation.MOD_ID, "textures/gui/root_filter_no_priority.png");
+    private static final int UPDATE_INTERVAL = 20;
 
-	private final TextureAtlasRegion textureSeparator;
+    private final TextureAtlasRegion textureSeparator;
 
-	private int ticksSinceUpdate;
-	private int lastSentPriority;
+    private int ticksSinceUpdate;
+    private int lastSentPriority;
 
-	public GuiRootFilter(EntityPlayer player, TileEntity tileEntity) {
-		this(new ContainerRootFilter(player, tileEntity));
-	}
+    public GuiRootFilter(EntityPlayer player, TileEntity tileEntity) {
+        this(new ContainerRootFilter(player, tileEntity));
+    }
 
-	public GuiRootFilter(ContainerRootFilter container) {
-		super(container);
+    public GuiRootFilter(ContainerRootFilter container) {
+        super(container);
 
-		ySize = 210;
+        if (container.hasSortingInventory()) {
+            ySize = 210;
+        }
 
-		final GuiFilterSlot[] filterSlots = new GuiFilterSlot[3];
-		final GuiDeleteFilterButton[] deleteButtons = new GuiDeleteFilterButton[3];
-		final GuiWhitelistButton[] whitelistButtons = new GuiWhitelistButton[3];
+        final GuiFilterSlot[] filterSlots = new GuiFilterSlot[3];
+        final GuiDeleteFilterButton[] deleteButtons = new GuiDeleteFilterButton[3];
+        final GuiWhitelistButton[] whitelistButtons = new GuiWhitelistButton[3];
 
-		int x = 10;
-		for(int i = 0; i < filterSlots.length; i++) {
-			filterSlots[i] = new GuiFilterSlot(this, container.getRootFilter(), i);
-			filterSlots[i].setPosition(x, 30);
-			rootNode.addChild(filterSlots[i]);
+        int x = 10;
+        for (int i = 0; i < filterSlots.length; i++) {
+            filterSlots[i] = new GuiFilterSlot(this, container.getRootFilter(), i);
+            filterSlots[i].setPosition(x, 30);
+            rootNode.addChild(filterSlots[i]);
 
-			deleteButtons[i] = new GuiDeleteFilterButton(x + 19, 27, filterSlots[i]);
-			rootNode.addChild(deleteButtons[i]);
+            deleteButtons[i] = new GuiDeleteFilterButton(x + 19, 27, filterSlots[i]);
+            rootNode.addChild(deleteButtons[i]);
 
-			whitelistButtons[i] = new GuiWhitelistButton(x + 1, 55, this, filterSlots[i]);
-			rootNode.addChild(whitelistButtons[i]);
-			x += 40;
-		}
+            whitelistButtons[i] = new GuiWhitelistButton(x + 1, 55, this, filterSlots[i]);
+            rootNode.addChild(whitelistButtons[i]);
+            x += 40;
+        }
 
-		ITileGuiHandler tileGuiHandler = InternalMethodsImpl.getGuiHandler(container.getTileEntity().getClass());
-		if(tileGuiHandler != null) {
-			GuiImageButton btnReturn = new GuiImageButton(guiLeft + xSize - 20, guiTop + 4, "chest_button") {
-				@Override
-				public void actionPerformed(int mouseButton) {
-					if(onGuiAboutToClose()) {
-						NetworkHandler.wrapper.sendToServer(new MessageReturnGUI());
-					}
-				}
-			};
-			rootNode.addChild(btnReturn);
-		}
+        ITileGuiHandler tileGuiHandler = InternalMethodsImpl.getGuiHandler(container.getTileEntity().getClass());
+        if (tileGuiHandler != null) {
+            GuiImageButton btnReturn = new GuiImageButton(guiLeft + xSize - 20, guiTop + 4, "chest_button") {
+                @Override
+                public void actionPerformed(int mouseButton) {
+                    if (onGuiAboutToClose()) {
+                        NetworkHandler.wrapper.sendToServer(new MessageReturnGUI());
+                    }
+                }
+            };
+            rootNode.addChild(btnReturn);
+        }
 
-		rootNode.addChild(new GuiLabel(10, 65, I18n.format("gui.refinedrelocation:root_filter.priority_label"), 0x404040));
-		rootNode.addChild(new GuiButtonPriority(10, 80, 100, 20, container.getSortingInventory()));
+        if (container.hasSortingInventory()) {
+            rootNode.addChild(new GuiLabel(10, 65, I18n.format("gui.refinedrelocation:root_filter.priority_label"), 0x404040));
+            rootNode.addChild(new GuiButtonPriority(10, 80, 100, 20, container.getSortingInventory()));
+        }
 
-		textureSeparator = ClientProxy.TEXTURE_ATLAS.getSprite("refinedrelocation:filter_separator");
-	}
+        textureSeparator = ClientProxy.TEXTURE_ATLAS.getSprite("refinedrelocation:filter_separator");
+    }
 
-	@Override
-	public void updateScreen() {
-		super.updateScreen();
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
 
-		ticksSinceUpdate++;
-		if(ticksSinceUpdate >= UPDATE_INTERVAL) {
-			ISortingInventory sortingInventory = container.getSortingInventory();
-			if(lastSentPriority != sortingInventory.getPriority()) {
-				RefinedRelocationAPI.sendContainerMessageToServer(ContainerRootFilter.KEY_PRIORITY, sortingInventory.getPriority());
-				lastSentPriority = sortingInventory.getPriority();
-			}
-			ticksSinceUpdate = 0;
-		}
-	}
+        ticksSinceUpdate++;
+        if (ticksSinceUpdate >= UPDATE_INTERVAL) {
+            ISortingInventory sortingInventory = container.getSortingInventory();
+            if (lastSentPriority != sortingInventory.getPriority()) {
+                RefinedRelocationAPI.sendContainerMessageToServer(ContainerRootFilter.KEY_PRIORITY, sortingInventory.getPriority());
+                lastSentPriority = sortingInventory.getPriority();
+            }
+            ticksSinceUpdate = 0;
+        }
+    }
 
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		drawDefaultBackground();
-		super.drawScreen(mouseX, mouseY, partialTicks);
-		renderHoveredToolTip(mouseX, mouseY);
-	}
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        renderHoveredToolTip(mouseX, mouseY);
+    }
 
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		GlStateManager.color(1f, 1f, 1f, 1f);
-		mc.getTextureManager().bindTexture(TEXTURE);
-		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+        GlStateManager.color(1f, 1f, 1f, 1f);
+        if (container.hasSortingInventory()) {
+            mc.getTextureManager().bindTexture(TEXTURE);
+        } else {
+            mc.getTextureManager().bindTexture(TEXTURE_NO_PRIORITY);
+        }
 
-		final int x = guiLeft + 10;
-		final int y = guiTop + 37;
-		GlStateManager.enableBlend();
-		textureSeparator.draw(x + 30, y, zLevel);
-		textureSeparator.draw(x + 70, y, zLevel);
-		GlStateManager.disableBlend();
+        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
 
-		super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
-	}
+        final int x = guiLeft + 10;
+        final int y = guiTop + 37;
+        GlStateManager.enableBlend();
+        textureSeparator.draw(x + 30, y, zLevel);
+        textureSeparator.draw(x + 70, y, zLevel);
+        GlStateManager.disableBlend();
 
-	@Override
-	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
+        super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+    }
 
-		String tileDisplayName = container.getTileEntity().getDisplayName().getUnformattedText();
-		fontRenderer.drawString(tileDisplayName.isEmpty() ? I18n.format("container.refinedrelocation:root_filter") : I18n.format("container.refinedrelocation:root_filter_with_name", tileDisplayName), 8, 6, 4210752);
-		fontRenderer.drawString(I18n.format("container.inventory"), 8, ySize - 96 + 2, 4210752);
-	}
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY);
 
-	@Override
-	public boolean onGuiAboutToClose() {
-		RefinedRelocationAPI.sendContainerMessageToServer(ContainerRootFilter.KEY_PRIORITY, container.getSortingInventory().getPriority());
-		return true;
-	}
+        String tileDisplayName = container.getTileEntity().getDisplayName().getUnformattedText();
+        fontRenderer.drawString(tileDisplayName.isEmpty() ? I18n.format("container.refinedrelocation:root_filter") : I18n.format("container.refinedrelocation:root_filter_with_name", tileDisplayName), 8, 6, 4210752);
+        fontRenderer.drawString(I18n.format("container.inventory"), 8, ySize - 96 + 2, 4210752);
+    }
+
+    @Override
+    public boolean onGuiAboutToClose() {
+        RefinedRelocationAPI.sendContainerMessageToServer(ContainerRootFilter.KEY_PRIORITY, container.getSortingInventory().getPriority());
+        return true;
+    }
 
 }
--- src/main/java/net/blay09/mods/refinedrelocation/container/ContainerRootFilter.java
+++ src/main/java/net/blay09/mods/refinedrelocation/container/ContainerRootFilter.java
@@ -26,207 +26,213 @@ import javax.annotation.Nullable;
 
 public class ContainerRootFilter extends ContainerMod {
 
-	public static final String KEY_ROOT_FILTER = "RootFilter";
-	public static final String KEY_ADD_FILTER = "AddFilter";
-	public static final String KEY_EDIT_FILTER = "EditFilter";
-	public static final String KEY_DELETE_FILTER = "DeleteFilter";
-	public static final String KEY_PRIORITY = "Priority";
-	public static final String KEY_BLACKLIST = "Blacklist";
-	public static final String KEY_BLACKLIST_INDEX = "FilterIndex";
+    public static final String KEY_ROOT_FILTER = "RootFilter";
+    public static final String KEY_ADD_FILTER = "AddFilter";
+    public static final String KEY_EDIT_FILTER = "EditFilter";
+    public static final String KEY_DELETE_FILTER = "DeleteFilter";
+    public static final String KEY_PRIORITY = "Priority";
+    public static final String KEY_BLACKLIST = "Blacklist";
+    public static final String KEY_BLACKLIST_INDEX = "FilterIndex";
 
-	private final EntityPlayer entityPlayer;
-	private final TileEntity tileEntity;
-	private final IRootFilter rootFilter;
+    private final EntityPlayer entityPlayer;
+    private final TileEntity tileEntity;
+    private final IRootFilter rootFilter;
 
-	private ReturnCallback returnCallback;
-	private ISortingInventory sortingInventory;
+    private ReturnCallback returnCallback;
+    private ISortingInventory sortingInventory;
 
-	private int lastFilterCount = -1;
-	private int lastPriority;
-	private final boolean[] lastBlacklist = new boolean[3];
+    private int lastFilterCount = -1;
+    private int lastPriority;
+    private final boolean[] lastBlacklist = new boolean[3];
 
-	public ContainerRootFilter(EntityPlayer player, TileEntity tileEntity) {
-		this(player, tileEntity, tileEntity.getCapability(CapabilityRootFilter.CAPABILITY, null));
-	}
+    public ContainerRootFilter(EntityPlayer player, TileEntity tileEntity) {
+        this(player, tileEntity, tileEntity.getCapability(CapabilityRootFilter.CAPABILITY, null));
+    }
 
-	public ContainerRootFilter(EntityPlayer player, TileEntity tileEntity, @Nullable IRootFilter rootFilter) {
-		this.entityPlayer = player;
-		this.tileEntity = tileEntity;
-		if(rootFilter == null) {
-			rootFilter = new RootFilter();
-		}
-		this.rootFilter = rootFilter;
-		sortingInventory = tileEntity.getCapability(CapabilitySortingInventory.CAPABILITY, null);
+    public ContainerRootFilter(EntityPlayer player, TileEntity tileEntity, @Nullable IRootFilter rootFilter) {
+        this.entityPlayer = player;
+        this.tileEntity = tileEntity;
+        if (rootFilter == null) {
+            rootFilter = new RootFilter();
+        }
 
-		addPlayerInventory(player, 128);
-	}
+        this.rootFilter = rootFilter;
+        sortingInventory = tileEntity.getCapability(CapabilitySortingInventory.CAPABILITY, null);
 
-	@Override
-	public void detectAndSendChanges() {
-		super.detectAndSendChanges();
+        addPlayerInventory(player, 128);
+    }
 
-		if(rootFilter.getFilterCount() != lastFilterCount) {
-			syncFilterList();
-			RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
-		}
+    @Override
+    public void detectAndSendChanges() {
+        super.detectAndSendChanges();
 
-		for(int i = 0; i < lastBlacklist.length; i++) {
-			boolean nowBlacklist = rootFilter.isBlacklist(i);
-			if(lastBlacklist[i] != nowBlacklist) {
-				NBTTagCompound compound = new NBTTagCompound();
-				compound.setInteger(KEY_BLACKLIST_INDEX, i);
-				compound.setBoolean(KEY_BLACKLIST, nowBlacklist);
-				RefinedRelocationAPI.syncContainerValue(KEY_BLACKLIST, compound, listeners);
-				lastBlacklist[i] = nowBlacklist;
-			}
-		}
+        if (rootFilter.getFilterCount() != lastFilterCount) {
+            syncFilterList();
+            RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
+        }
 
-		if(sortingInventory != null && sortingInventory.getPriority() != lastPriority) {
-			RefinedRelocationAPI.syncContainerValue(KEY_PRIORITY, sortingInventory.getPriority(), listeners);
-			lastPriority = sortingInventory.getPriority();
-		}
-	}
+        for (int i = 0; i < lastBlacklist.length; i++) {
+            boolean nowBlacklist = rootFilter.isBlacklist(i);
+            if (lastBlacklist[i] != nowBlacklist) {
+                NBTTagCompound compound = new NBTTagCompound();
+                compound.setInteger(KEY_BLACKLIST_INDEX, i);
+                compound.setBoolean(KEY_BLACKLIST, nowBlacklist);
+                RefinedRelocationAPI.syncContainerValue(KEY_BLACKLIST, compound, listeners);
+                lastBlacklist[i] = nowBlacklist;
+            }
+        }
 
-	@Override
-	public ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player) {
-		ItemStack itemStack = super.slotClick(slotId, dragType, clickTypeIn, player);
-		RefinedRelocationAPI.updateFilterPreview(player, tileEntity, rootFilter);
-		return itemStack;
-	}
+        if (sortingInventory != null && sortingInventory.getPriority() != lastPriority) {
+            RefinedRelocationAPI.syncContainerValue(KEY_PRIORITY, sortingInventory.getPriority(), listeners);
+            lastPriority = sortingInventory.getPriority();
+        }
+    }
 
-	private void syncFilterList() {
-		NBTTagCompound tagCompound = new NBTTagCompound();
-		tagCompound.setTag(KEY_ROOT_FILTER, rootFilter.serializeNBT());
-		RefinedRelocationAPI.syncContainerValue(KEY_ROOT_FILTER, tagCompound, listeners);
-		lastFilterCount = rootFilter.getFilterCount();
-		for(int i = 0; i < lastBlacklist.length; i++) {
-			lastBlacklist[i] = rootFilter.isBlacklist(i);
-		}
-	}
+    @Override
+    public ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player) {
+        ItemStack itemStack = super.slotClick(slotId, dragType, clickTypeIn, player);
+        RefinedRelocationAPI.updateFilterPreview(player, tileEntity, rootFilter);
+        return itemStack;
+    }
 
-	public TileEntity getTileEntity() {
-		return tileEntity;
-	}
+    private void syncFilterList() {
+        NBTTagCompound tagCompound = new NBTTagCompound();
+        tagCompound.setTag(KEY_ROOT_FILTER, rootFilter.serializeNBT());
+        RefinedRelocationAPI.syncContainerValue(KEY_ROOT_FILTER, tagCompound, listeners);
+        lastFilterCount = rootFilter.getFilterCount();
+        for (int i = 0; i < lastBlacklist.length; i++) {
+            lastBlacklist[i] = rootFilter.isBlacklist(i);
+        }
+    }
 
-	@Override
-	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
-		ItemStack itemStack = ItemStack.EMPTY;
-		Slot slot = inventorySlots.get(index);
-		if (slot != null && slot.getHasStack()) {
-			ItemStack slotStack = slot.getStack();
-			itemStack = slotStack.copy();
+    public TileEntity getTileEntity() {
+        return tileEntity;
+    }
 
-			if (index < 27) {
-				if (!mergeItemStack(slotStack, 27, inventorySlots.size(), true)) {
-					return ItemStack.EMPTY;
-				}
-			} else if (!mergeItemStack(slotStack, 0, 27, false)) {
-				return ItemStack.EMPTY;
-			}
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+        ItemStack itemStack = ItemStack.EMPTY;
+        Slot slot = inventorySlots.get(index);
+        if (slot != null && slot.getHasStack()) {
+            ItemStack slotStack = slot.getStack();
+            itemStack = slotStack.copy();
 
-			if (slotStack.isEmpty()) {
-				slot.putStack(ItemStack.EMPTY);
-			} else {
-				slot.onSlotChanged();
-			}
-		}
+            if (index < 27) {
+                if (!mergeItemStack(slotStack, 27, inventorySlots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!mergeItemStack(slotStack, 0, 27, false)) {
+                return ItemStack.EMPTY;
+            }
 
-		return itemStack;
-	}
+            if (slotStack.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+        }
 
-	@Override
-	public void receivedMessageServer(IContainerMessage message) {
-		if(message.getKey().equals(KEY_ADD_FILTER)) {
-			String typeId = message.getStringValue();
-			IFilter filter = FilterRegistry.createFilter(typeId);
-			if(filter == null) {
-				// Client tried to create a filter that doesn't exist. Bad client!
-				return;
-			}
-			if(rootFilter.getFilterCount() >= 3) {
-				// Client tried to create more than three filters. Bad client!
-				return;
-			}
-			rootFilter.addFilter(filter);
-			tileEntity.markDirty();
-			lastFilterCount = rootFilter.getFilterCount();
-			syncFilterList();
-			RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
-			RefinedRelocation.proxy.openGui(entityPlayer, new MessageOpenGui(GuiHandler.GUI_ANY_FILTER, tileEntity.getPos(), rootFilter.getFilterCount() - 1));
-		} else if(message.getKey().equals(KEY_EDIT_FILTER)) {
-			int index = message.getIntValue();
-			if(index < 0 || index >= rootFilter.getFilterCount()) {
-				// Client tried to edit a filter that doesn't exist. Bad client!
-				return;
-			}
-			IFilter filter = rootFilter.getFilter(index);
-			if(filter != null) {
-				RefinedRelocation.proxy.openGui(entityPlayer, new MessageOpenGui(GuiHandler.GUI_ANY_FILTER, tileEntity.getPos(), index));
-			}
-		} else if(message.getKey().equals(KEY_DELETE_FILTER)) {
-			int index = message.getIntValue();
-			if(index < 0 || index >= rootFilter.getFilterCount()) {
-				// Client tried to delete a filter that doesn't exist. Bad client!
-				return;
-			}
-			rootFilter.removeFilter(index);
-			tileEntity.markDirty();
-		} else if(message.getKey().equals(KEY_PRIORITY)) {
-			int value = message.getIntValue();
-			if(value < Priority.LOWEST || value > Priority.HIGHEST) {
-				// Client tried to set an invalid priority. Bad client!
-				return;
-			}
-			if(sortingInventory == null) {
-				// Client tried to set priority on an invalid tile. Bad client!
-				return;
-			}
-			sortingInventory.setPriority(value);
-			tileEntity.markDirty();
-		} else if(message.getKey().equals(KEY_BLACKLIST)) {
-			NBTTagCompound tagCompound = message.getNBTValue();
-			int index = tagCompound.getInteger(KEY_BLACKLIST_INDEX);
-			if(index < 0 || index >= rootFilter.getFilterCount()) {
-				// Client tried to delete a filter that doesn't exist. Bad client!
-				return;
-			}
-			rootFilter.setIsBlacklist(index, tagCompound.getBoolean(KEY_BLACKLIST));
-			tileEntity.markDirty();
-			RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
-		}
-	}
+        return itemStack;
+    }
 
-	@Override
-	public void receivedMessageClient(IContainerMessage message) {
-		if(message.getKey().equals(KEY_ROOT_FILTER)) {
-			rootFilter.deserializeNBT(message.getNBTValue().getCompoundTag(KEY_ROOT_FILTER));
-		} else if(message.getKey().equals(KEY_PRIORITY)) {
-			getSortingInventory().setPriority(message.getIntValue());
-		} else if(message.getKey().equals(KEY_BLACKLIST)) {
-			NBTTagCompound compound = message.getNBTValue();
-			rootFilter.setIsBlacklist(compound.getInteger(KEY_BLACKLIST_INDEX), compound.getBoolean(KEY_BLACKLIST));
-		}
-	}
+    @Override
+    public void receivedMessageServer(IContainerMessage message) {
+        if (message.getKey().equals(KEY_ADD_FILTER)) {
+            String typeId = message.getStringValue();
+            IFilter filter = FilterRegistry.createFilter(typeId);
+            if (filter == null) {
+                // Client tried to create a filter that doesn't exist. Bad client!
+                return;
+            }
+            if (rootFilter.getFilterCount() >= 3) {
+                // Client tried to create more than three filters. Bad client!
+                return;
+            }
+            rootFilter.addFilter(filter);
+            tileEntity.markDirty();
+            lastFilterCount = rootFilter.getFilterCount();
+            syncFilterList();
+            RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
+            RefinedRelocation.proxy.openGui(entityPlayer, new MessageOpenGui(GuiHandler.GUI_ANY_FILTER, tileEntity.getPos(), rootFilter.getFilterCount() - 1));
+        } else if (message.getKey().equals(KEY_EDIT_FILTER)) {
+            int index = message.getIntValue();
+            if (index < 0 || index >= rootFilter.getFilterCount()) {
+                // Client tried to edit a filter that doesn't exist. Bad client!
+                return;
+            }
+            IFilter filter = rootFilter.getFilter(index);
+            if (filter != null) {
+                RefinedRelocation.proxy.openGui(entityPlayer, new MessageOpenGui(GuiHandler.GUI_ANY_FILTER, tileEntity.getPos(), index));
+            }
+        } else if (message.getKey().equals(KEY_DELETE_FILTER)) {
+            int index = message.getIntValue();
+            if (index < 0 || index >= rootFilter.getFilterCount()) {
+                // Client tried to delete a filter that doesn't exist. Bad client!
+                return;
+            }
+            rootFilter.removeFilter(index);
+            tileEntity.markDirty();
+        } else if (message.getKey().equals(KEY_PRIORITY)) {
+            int value = message.getIntValue();
+            if (value < Priority.LOWEST || value > Priority.HIGHEST) {
+                // Client tried to set an invalid priority. Bad client!
+                return;
+            }
+            if (sortingInventory == null) {
+                // Client tried to set priority on an invalid tile. Bad client!
+                return;
+            }
+            sortingInventory.setPriority(value);
+            tileEntity.markDirty();
+        } else if (message.getKey().equals(KEY_BLACKLIST)) {
+            NBTTagCompound tagCompound = message.getNBTValue();
+            int index = tagCompound.getInteger(KEY_BLACKLIST_INDEX);
+            if (index < 0 || index >= rootFilter.getFilterCount()) {
+                // Client tried to delete a filter that doesn't exist. Bad client!
+                return;
+            }
+            rootFilter.setIsBlacklist(index, tagCompound.getBoolean(KEY_BLACKLIST));
+            tileEntity.markDirty();
+            RefinedRelocationAPI.updateFilterPreview(entityPlayer, tileEntity, rootFilter);
+        }
+    }
 
-	public IRootFilter getRootFilter() {
-		return rootFilter;
-	}
+    @Override
+    public void receivedMessageClient(IContainerMessage message) {
+        if (message.getKey().equals(KEY_ROOT_FILTER)) {
+            rootFilter.deserializeNBT(message.getNBTValue().getCompoundTag(KEY_ROOT_FILTER));
+        } else if (message.getKey().equals(KEY_PRIORITY)) {
+            getSortingInventory().setPriority(message.getIntValue());
+        } else if (message.getKey().equals(KEY_BLACKLIST)) {
+            NBTTagCompound compound = message.getNBTValue();
+            rootFilter.setIsBlacklist(compound.getInteger(KEY_BLACKLIST_INDEX), compound.getBoolean(KEY_BLACKLIST));
+        }
+    }
 
-	public ISortingInventory getSortingInventory() {
-		if(sortingInventory == null) {
-			// Create a dummy sorting inventory for the client to store its settings.
-			sortingInventory = new SortingInventory();
-		}
-		return sortingInventory;
-	}
+    public IRootFilter getRootFilter() {
+        return rootFilter;
+    }
 
-	@Nullable
-	public ReturnCallback getReturnCallback() {
-		return returnCallback;
-	}
+    public boolean hasSortingInventory() {
+        return tileEntity.hasCapability(CapabilitySortingInventory.CAPABILITY, null);
+    }
 
-	public ContainerRootFilter setReturnCallback(@Nullable ReturnCallback returnCallback) {
-		this.returnCallback = returnCallback;
-		return this;
-	}
+    public ISortingInventory getSortingInventory() {
+        if (sortingInventory == null) {
+            // Create a dummy sorting inventory for the client to store its settings.
+            sortingInventory = new SortingInventory();
+        }
+
+        return sortingInventory;
+    }
+
+    @Nullable
+    public ReturnCallback getReturnCallback() {
+        return returnCallback;
+    }
+
+    public ContainerRootFilter setReturnCallback(@Nullable ReturnCallback returnCallback) {
+        this.returnCallback = returnCallback;
+        return this;
+    }
 }
