3ca4c45bd089d63a0772e995a951a49a065c4e9c
blay09 <blay09@hotmail.de>
2020-04-05 11:13:01 +0200

    Require shift-confirmation when deleting a filter
    #54

--- src/main/java/net/blay09/mods/refinedrelocation/client/gui/element/GuiDeleteFilterButton.java
+++ src/main/java/net/blay09/mods/refinedrelocation/client/gui/element/GuiDeleteFilterButton.java
@@ -6,12 +6,15 @@ import net.blay09.mods.refinedrelocation.client.gui.base.ITickableElement;
 import net.blay09.mods.refinedrelocation.client.gui.base.ITooltipElement;
 import net.blay09.mods.refinedrelocation.client.gui.base.element.GuiImageButton;
 import net.blay09.mods.refinedrelocation.container.RootFilterContainer;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
 
 import java.util.List;
 
 public class GuiDeleteFilterButton extends GuiImageButton implements ITickableElement, ITooltipElement {
+
+    private static boolean shiftGuard;
 
     private final GuiFilterSlot parentSlot;
 
@@ -25,16 +28,42 @@ public class GuiDeleteFilterButton extends GuiImageButton implements ITickableEl
     @Override
     public void tick() {
         visible = parentSlot.hasFilter();
+
+        boolean isShiftDown = Screen.hasShiftDown();
+        active = !shiftGuard && isShiftDown;
+        if (!isShiftDown) {
+            shiftGuard = false;
+        }
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (isMouseOver(mouseX, mouseY) && !active) {
+            return true;
+        }
+
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean isMouseOver(double mouseX, double mouseY) {
+        return this.visible && mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
     }
 
     @Override
     public void onClick(double mouseX, double mouseY) {
+        shiftGuard = true;
         RefinedRelocationAPI.sendContainerMessageToServer(RootFilterContainer.KEY_DELETE_FILTER, parentSlot.getFilterIndex());
     }
 
     @Override
     public void addTooltip(List<String> list) {
         list.add(TextFormatting.RED + I18n.format("gui.refinedrelocation:root_filter.delete_filter"));
+        if (!active) {
+            list.add(TextFormatting.YELLOW + I18n.format("gui.refinedrelocation:root_filter.hold_shift_to_delete"));
+        } else {
+            list.add(TextFormatting.YELLOW + I18n.format("gui.refinedrelocation:root_filter.delete_cannot_be_undone"));
+        }
     }
 
 }
